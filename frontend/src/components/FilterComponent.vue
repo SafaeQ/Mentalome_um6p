32581b012cd2db5296ccd5525b44d093103434c6
SafaeQ <safaedani009@gmail.com>
2023-08-26 17:20:27 +0100

    ♻️ refactor

--- frontend/src/components/FilterComponent.vue
+++ frontend/src/components/FilterComponent.vue
@@ -9,84 +9,85 @@
           </h2>
         </div>
         <div class="card p-3">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="form-group">
-                  <label for="gene_ids" class="form-label">Select gene ids:</label>
-                  <div class="badge-select">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label for="gene_ids" class="form-label">Select gene ids:</label>
+                <div class="badge-select">
+                  <div
+                    id="gene_ids"
+                    class="form-control form-select d-flex input"
+                    data-name="select-input"
+                    @click="toggleListe($event)"
+                  >
                     <div
-                      id="gene_ids"
-                      class="form-control form-select d-flex input"
-                      data-name="select-input"
-                      @click="toggleListe($event)"
+                      class="badge bg-light shadow-sm d-inline-block"
+                      v-for="id in filter.selectedGeneIds"
                     >
-                      <div
-                        class="badge bg-light shadow-sm d-inline-block"
-                        v-for="id in filter.selectedGeneIds"
-                      >
-                        <span class="text content">{{ id }}</span>
-                        <span class="close" @click="unSelectGeneId(id)">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="10"
-                            height="10"
-                            fill="currentColor"
-                            class="bi bi-x-lg"
-                            viewBox="0 0 16 16"
-                          >
-                            <path
-                              d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
-                            /></svg
-                        ></span>
-                      </div>
+                      <span class="text content">{{ id }}</span>
+                      <span class="close" @click="unSelectGeneId(id)">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="10"
+                          height="10"
+                          fill="currentColor"
+                          class="bi bi-x-lg"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
+                          />
+                        </svg>
+                      </span>
                     </div>
-                    <div class="list card show" v-if="showList">
-                      <input type="text" class="form-control search" v-model="geneIdsFilter" />
-                      <div class="items">
-                        <li class="nav-link" @click="selectGeneId(id)" v-for="id in filtredGenIds">
-                          {{ id }}
-                        </li>
-                      </div>
+                  </div>
+                  <div class="list card show" v-if="showList">
+                    <input type="text" class="form-control search" v-model="geneIdsFilter" />
+                    <div class="items">
+                      <li class="nav-link" @click="selectGeneId(id)" v-for="id in filtredGenIds">
+                        {{ id }}
+                      </li>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label for="gene_ids" class="form-label">Select Disease:</label>
-                  <select id="gene_ids" v-model="filter.disease" class="form-control form-select">
-                    <option v-bind:value="disease" v-for="disease in diseases">
-                      {{ disease }}
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label for="gene_ids" class="form-label">Select Expriment:</label>
-                  <select id="gene_ids" class="form-control form-select" v-model="filter.expriment">
-                    <option value="">All</option>
-                    <option v-bind:value="expriment" v-for="expriment in expriments">
-                      {{ expriment }}
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-group">
-                  <label for="gene_ids" class="form-label">Select SRA:</label>
-                  <select id="gene_ids" class="form-control form-select" v-model="filter.sra">
-                    <option value="">All</option>
-                    <option v-bind:value="sra" v-for="sra in sras">{{ sra }}</option>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-12 text-center">
-                <button @click="search()" class="btn btn-primary btn-lg px-5 shadow mb-3">
-                  Apply filter
-                </button>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="gene_ids" class="form-label">Select Disease:</label>
+                <select id="gene_ids" v-model="filter.disease" class="form-control form-select">
+                  <option v-bind:value="disease" v-for="disease in diseases">
+                    {{ disease }}
+                  </option>
+                </select>
               </div>
             </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="gene_ids" class="form-label">Select Expriment:</label>
+                <select id="gene_ids" class="form-control form-select" v-model="filter.expriment">
+                  <option value="">All</option>
+                  <option v-bind:value="expriment" v-for="expriment in expriments">
+                    {{ expriment }}
+                  </option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="gene_ids" class="form-label">Select SRA:</label>
+                <select id="gene_ids" class="form-control form-select" v-model="filter.sra">
+                  <option value="">All</option>
+                  <option v-bind:value="sra" v-for="sra in sras">{{ sra }}</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-12 text-center">
+              <button @click="search()" class="btn btn-primary btn-lg px-5 shadow mb-3">
+                Apply filter
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -105,7 +106,7 @@ export default {
         key: 1
       },
       geneIdsFilter: '',
-      filtredGenIds:[],
+      filtredGenIds: [],
       geneIds: '',
       expriments: '',
       sras: '',
@@ -142,7 +143,7 @@ export default {
 
   watch: {
     geneIdsFilter(keyword) {
-      this.filterGeneIds(keyword);
+      this.filterGeneIds(keyword)
     }
   },
 
@@ -178,9 +179,9 @@ export default {
         key: ++this.filter.key
       })
     },
-    filterGeneIds: function (keyword){
-      this.filtredGenIds = this.geneIds.filter(e=>{
-        return e.toLowerCase().includes(keyword.toLowerCase());
+    filterGeneIds: function (keyword) {
+      this.filtredGenIds = this.geneIds.filter((e) => {
+        return e.toLowerCase().includes(keyword.toLowerCase())
       })
     }
   }
@@ -205,14 +206,14 @@ export default {
 
 .badge-select .input {
   padding: 0.9rem 2.25rem 0.9rem 1rem;
-flex-wrap: wrap;
+  flex-wrap: wrap;
 }
 
 .badge-select .badge {
   padding: 0.5rem;
   color: #000;
   font-size: 1rem;
-  margin: .5rem;
+  margin: 0.5rem;
 }
 
 .badge-select .badge .close {
